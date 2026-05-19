251e411c5e77c274dc71e50c2f40861a9b09d12b
Urooj Nisar <urooj.nisar007@gmail.com>
2026-05-19 21:30:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Login logic yahan aayegi
+    window.location.href = 'dashboard.html'; // Login ke baad dashboard par bhejne ke liye
+});
